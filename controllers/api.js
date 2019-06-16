6f14d252162d4839dfe0074d2bc185aede614a2f
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-06-16 08:48:43 -0400

    I am able to scrape and get the most popular movies as json but having trouble rendering!

--- controllers/api.js
+++ controllers/api.js
@@ -5,17 +5,23 @@ const request = require("request"); //Makes http calls
 const cheerio = require("cheerio");
 var axios = require("axios");
 
-
+// router.get("/", function(req, res) {
+//     res.redirect("/movies");
+//   });
 
 // A GET route for scraping the IMDB website
 router.get("/scrape", (req, res) => {
     // console.log("scrape ran")
     // First, we grab the body of the html with axios
   axios.get("https://www.imdb.com/chart/moviemeter?ref_=nv_mv_mpm").then(function(response) {
+    //   console.log(response)
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
+    // console.log(response.data)
+    // console.log($('.titleColumn'))
             // Now, we grab every article:
-            $('td .titleColumn').each(function (i, element) {
+            $(".titleColumn").each(function (i, element) {
+// console.log(".titleColumn")
                 // Save an empty result object
                 // let count = i;
                 let result = {};
@@ -28,7 +34,7 @@ router.get("/scrape", (req, res) => {
                     .attr("href");
                 
                 
-           // Create a new Article using the `result` object built from scraping
+           // Create a new Movie using the `result` object built from scraping
       db.Movie.create(result)
       .then(function(dbMovie) {
         // View the added result in the console
@@ -42,10 +48,26 @@ router.get("/scrape", (req, res) => {
 
   // Send a message to the client
   res.send("Scrape Complete");
-});
+//   res.redirect("/");
+
+  
 });
 
+});
 
+// Route for getting all Movies from the db
+router.get("/movies", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Movie.find({})
+      .then(function(dbMovie) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbMovie);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
 // app.get("/", function(req, res) {
 //     res.render("index");
 // });
